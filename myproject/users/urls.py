e59814a44d7d13d5454ba3e9d2248def897bf212
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2025-03-06 03:58:28 -0500

    register users app added

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -15,18 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
+app_name = 'users'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.homepage ),
-    path('about/', views.about),
-    path('posts/', include('posts.urls')),
-    path('users/', include('users.urls'))
+    path('register', views.register_view, name = 'register'),
+    
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

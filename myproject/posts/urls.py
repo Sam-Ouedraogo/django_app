2143c1ef4e53aec500fef7f54198b4b7f633d1d1
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2025-03-07 11:18:58 -0500

    hide login and registration when user is logged in

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -22,6 +22,8 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name = 'list'),
+    path('new-post/', views.post_new, name = 'new-post'),
     path('<slug:slug>', views.post_page, name = 'page'),
+    
     
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 #from django.http import HttpResponse
 # Create your views here.
 
@@ -12,4 +13,9 @@ def post_page(request, slug):
 #    return HttpResponse(slug)
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post' : post})
+
+@login_required(login_url="/users/login")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
+
     

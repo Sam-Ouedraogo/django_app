98e089c9c673347c20a129980fc28b9a4c84a31d
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2025-03-07 13:43:28 -0500

    create custom post form and collect static

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+#import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fig*1i3%4d#%kr+=lrcwkl5d&%qd16%5t9@r#h5xo$2igv5mk+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -119,14 +119,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
-
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_URL = BASE_DIR / 'assets'
+MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+    #os.path.join(BASE_DIR,'static')
+]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Post(models.Model):
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True) #dateTime stamp will be added every time a user enters a post
     banner = models.ImageField(default='profile_picture.jpg', blank=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE, default=None)
     
     #create method that will return the post title
     
